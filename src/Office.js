b1ffccf7369f0c6f93e01938478c1d8cf07c95cb
Ben Jones <benwjones76@outlook.com>
2020-11-19 21:58:18 +0000

    cleaned up logging

--- src/Office.js
+++ src/Office.js
@@ -12,12 +12,11 @@ class Office {
     availableRooms() {
         var freeRooms = [];
         this.rooms.forEach(checkAvailable);
-        console.log(this.rooms);
+
         function checkAvailable(room) {
-            console.log(room);
+            
             if (room.isAvailable()) {
                 freeRooms.push(room);
-                console.log('pants');
             }
         }
         return freeRooms;
